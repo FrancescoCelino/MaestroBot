5b3e086b45a59885b5596296b64072ff4cdee95e
FrancescoCelino <celinofrancesco849@gmail.com>
2025-05-19 22:18:15 +0200

    started fine_tuning.py and updated requirement.txt

---
+++ app/fine_tuning.py
@@ -0,0 +1,14 @@
+import torch
+from trl import SFTTrainer
+from datasets import load_dataset
+from transformers import TrainingArguments, TextStreamer
+from unsloth.chat_templates import get_chat_template
+from unsloth import FastLanguageModel, is_bfloat16_supported
+
+max_seq_length = 2048
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name="unsloth/Meta-Llama-3.1-8B-bnb-4bit",
+    max_seq_length=max_seq_length,
+    load_in_4bit=True,
+    dtype=None,
+)
